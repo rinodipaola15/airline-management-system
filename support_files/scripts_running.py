cf00858c78f5f88d9c293338b6b9374178027bea
Rino Di Paola <46008803+rinodipaola15@users.noreply.github.com>
2023-05-20 14:07:47 +0200

    Update scripts_running.py

--- support_files/scripts_running.py
+++ support_files/scripts_running.py
@@ -8,5 +8,8 @@ if __name__ == '__main__':
     metrics_fetching_process = subprocess.Popen(["python3", "metrics_fetching.py"])
     metrics_fetching_process.wait()
 
-    metrics_analysis = subprocess.Popen(["python3", "metrics_analysis.py"])
-    metrics_analysis.wait()
+    metrics_analysis_process = subprocess.Popen(["python3", "metrics_analysis.py"])
+    metrics_analysis_process.wait()
+    
+    distribution_graph_process = subprocess.Popen(["python3", "distribution_graph.py"])
+    distribution_graph_process.wait()
